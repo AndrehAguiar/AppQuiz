343f4f46b208ac746ab05f122801d07f62f42001
AndrehAguiar <andreaguiar.g@gmail.com>
2020-09-14 17:15:25 -0300

    Created high scores list

--- js/end.js
+++ js/end.js
@@ -2,6 +2,10 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 10;
+
+console.log(highScores);
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -12,4 +16,18 @@ username.addEventListener('keyup', () => {
 saveHighScore = e => {
     console.log("clicked the save button");
     e.preventDefault();
+
+    const score = {
+        id: highScores[0].id + 1,
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score || b.id - a.id);
+    highScores.splice(10);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+
+    console.log(highScores);
 };
